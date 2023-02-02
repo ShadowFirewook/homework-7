cf75547f69cf2c159cfb1929dcdaa852b29b9270
ShadowFirewook <shadowfirewook@gmail.com>
2023-02-02 16:03:03 +0600

    navController implementation in MainActivity

--- app/src/main/java/com/example/homework_7/presentation/MainActivity.kt
+++ app/src/main/java/com/example/homework_7/presentation/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.homework_7.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.example.homework_7.R
+import com.example.homework_7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController = findNavController(R.id.nav_host_fragment)
     }
 }
